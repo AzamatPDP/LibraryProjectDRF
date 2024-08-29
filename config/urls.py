4330b26993f9e3f73663dc87e93ec9a00012c276
master <jumabayevazamat8774@gmail.com>
2024-08-29 15:36:01 +0500

    update project

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,29 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Kitoblar do'koni API",
+        default_version='v1',
+        description="Online kitoblar do'koni uchun API",
+        terms_of_service="demo.com",
+        contact=openapi.Contact(email="jumabayevazamat8774@gmail.com"),
+        license=openapi.License(name="Demo licence TEST")
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny]
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include("library.urls"))
+    path('api/v1/', include("library.urls")),
 
+    # swagger
+    path('swagger/', schema_view.with_ui(
+        'swagger', cache_timeout=0), name='swagger-swagger-ui'),
+    path('redoc/', schema_view.with_ui(
+        'redoc', cache_timeout=0), name='schema-redoc')
 ]
